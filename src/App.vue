7d38ca9faf1f3cbe7f727a3a7a89098bd1a3a7a1
tosinezekiel <seunezekiel11@gmail.com>
2019-12-29 18:11:54 -0800

    updated dependencies...(vueNoty,vuex,vue-router,axios)

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Valuedseed App"/>
   </div>
 </template>
 
@@ -12,6 +12,9 @@ export default {
   name: 'app',
   components: {
     HelloWorld
+  },
+  created() {
+      //this.$noty.info("Your pizza has been saved!")
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import Vuex from 'vuex'
+import VueNoty from 'vuejs-noty'
 import App from './App.vue'
+import routes from  './routes'
+import store from  './store'
 
+Vue.use(VueNoty, {
+  timeout: 4000,
+  progressBar: true,
+  layout: 'topRight'
+})
+Vue.use(VueAxios, axios)
+Vue.use(Vuex)
+Vue.use(VueRouter)
 Vue.config.productionTip = false
-
+import 'vuejs-noty/dist/vuejs-noty.css'
 new Vue({
+  routes,
+  store,
   render: h => h(App),
 }).$mount('#app')
